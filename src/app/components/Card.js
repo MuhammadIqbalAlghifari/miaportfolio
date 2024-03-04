edc9a5cc1297f111c4da9848b5ccee91b79ee7cc
MuhammadIqbalAlghifari <iqbalalghifari18283@gmail.com>
2024-03-04 18:08:40 +0700

    link emyu updated

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -110,7 +110,7 @@ export default function Card() {
                                         <div className="text-sm rounded-md text-white text-center">A Knockout Kings clone. This website is about my favourite club at english premier league, Manchester United.</div>
                                             <div className="flex justify-center items-center gap-4">
                                                 <Tooltip label='Demo' fontSize='sm' className="bg-black dark:bg-slate-300 text-white dark:text-black">
-                                                    <a href="https://manchesterunited.vercel.app" rel="noopener noreferrer" target="_blank">
+                                                    <a href="https://manchesterunited-cyan.vercel.app/" rel="noopener noreferrer" target="_blank">
                                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 text-white">
                                                             <path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clipRule="evenodd" />
                                                         </svg>
